acc1a7530bf0d47caf121328e2297d16ee7e5cc4
eu.rebecagarcia <rebecagarciarodrigues21@gmail.com>
2022-06-27 19:47:37 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let familia = ['Rebeca', 'Gabriel', 'Emanuele', 'Heitor', 'Caleb'];
+console.log(familia);
+ 
+familia.unshift('Astolfo');
+console.log(familia);
+
+familia.pop();
+console.log(familia);
+
+familiailia.push( 'Caleb', 'Charlote');
+console.log(familia);
+
+familia.shift();
+console.log(familia);
+
+
+let numbers=[7,5,6,3,8,9,2,1,4]
+numbers.sort();
+console.log(numbers)
